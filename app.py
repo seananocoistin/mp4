85c6e0392e92df6ced8183172a5b67bcc4b4641a
Seanán Ó Coistín <seananoc@gmail.com>
2020-09-03 20:54:42 +0000

    Initial changes and preparation for the project

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
-
 author = ""
 app = Flask(__name__)
 app.config["MONGO_URI"]= os.environ.get('MONGO_URI')
